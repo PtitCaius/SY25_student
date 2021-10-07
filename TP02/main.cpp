a37920805480ed1e4e48705faf5735b8cae5d844
Marius Lopez <marius.lopez@utt.fr>
2021-10-07 17:13:13 +0200

    commit initial

---
+++ TP02/main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+int main(int argc, char *argv[])
+{
+    //Le but est de récupérer le flux vidéo de la caméra sur la raspberry pi, puis de l'afficher.
+    //Vous pouvez vous servir de cet exemple : https://riptutorial.com/opencv/example/21401/get-image-from-webcam
+
+    //////////////////
+    //LE CODE VA ICI//
+    //////////////////
+
+    return 0;
+}
